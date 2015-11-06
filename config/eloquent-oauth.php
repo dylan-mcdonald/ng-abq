7224d358629bd0168b7917ee22d78a52faa0a48c
Dylan McDonald <dmcdonald21@cnm.edu>
2015-11-05 22:13:32 -0700

    Enabled Facebook authentication

--- config/eloquent-oauth.php
+++ config/eloquent-oauth.php
@@ -7,7 +7,13 @@ return [
 			'client_id' => env('GITHUB_ID'),
 			'client_secret' => env('GITHUB_SECRET'),
 			'redirect_uri' => 'http://www.ng-abq.com/github/login',
-			'scope' => [],
+			'scope' => []
+		],
+		'facebook' => [
+			'client_id' => env('FACEBOOK_ID'),
+			'client_secret' => env('FACEBOOK_SECRET'),
+			'redirect_uri' => 'http://www.ng-abq.com/facebook/login',
+			'scope' => []
 		]
 	],
 ];
