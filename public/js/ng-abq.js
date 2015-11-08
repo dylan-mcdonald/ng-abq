1f9e1cd3e4d142e9c7f699ffea28d8a64faf819c
Dylan McDonald <dmcdonald21@cnm.edu>
2015-11-08 15:29:47 -0700

    Added Angular foundations

---
+++ public/js/ng-abq.js
@@ -0,0 +1,1 @@
+var app = angular.module("NgAbq", ["ngMessages", "ui.bootstrap"]);
--- resources/views/ng-abq.blade.php
+++ resources/views/ng-abq.blade.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
-<html>
+<html ng-app="NgAbq">
 	<head>
+		<!-- CSS stylesheets -->
 		<link type="text/css" rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" />
 		<link type="text/css" rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
 		<link type="text/css" rel="stylesheet" href="/css/ng-abq.css" />
+
+		<!-- CDN derived JavaScript -->
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.4.6/angular.min.js"></script>
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.4.6/angular-messages.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.14.3/ui-bootstrap-tpls.min.js"></script>
+
+		<!-- Site specific JavaScript -->
+		<script type="text/javascript" src="/js/ng-abq.js"></script>
 		<title>ng-abq | Albuquerque AngularJS Meetup Group</title>
 	</head>
 	<body>
