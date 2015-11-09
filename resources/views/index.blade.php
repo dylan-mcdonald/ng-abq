6407d39f204d881fb1047bbcc5a0459047d7a63a
Dylan McDonald <dmcdonald21@cnm.edu>
2015-11-08 18:49:41 -0700

    Completed sign up form validation

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,6 +18,9 @@
 						</div>
 						<input type="text" class="form-control" id="name" name="name" placeholder="What's your name?" ng-model="signupData.name" ng-required="true" />
 					</div>
+					<div class="alert alert-danger" role="alert" ng-messages="signupForm.name.$error" ng-if="signupForm.name.$touched" ng-hide="signupForm.name.$valid">
+						<p ng-message="required">Please enter your name.</p>
+					</div>
 				</div>
 				<div class="form-group" ng-class="{ 'has-error': signupForm.email.$touched && signupForm.email.$invalid }">
 					<label class="control-label" for="email">Email</label>
@@ -26,6 +29,10 @@
 							<i class="fa fa-envelope" aria-hidden="true"></i>
 						</div>
 						<input type="email" class="form-control" id="email" name="email" placeholder="What's your Email?" ng-model="signupData.email" ng-required="true" />
+					</div>
+					<div class="alert alert-danger" role="alert" ng-messages="signupForm.email.$error" ng-if="signupForm.email.$touched" ng-hide="signupForm.email.$valid">
+						<p ng-message="email">Email is invalid.</p>
+						<p ng-message="required">Please enter your Email.</p>
 					</div>
 				</div>
 				<div class="form-group" ng-class="{ 'has-error': signupForm.password.$touched && signupForm.password.$invalid }">
@@ -36,6 +43,10 @@
 						</div>
 						<input type="password" class="form-control" id="password" name="password" placeholder="Password&hellip;" ng-model="signupData.password" ng-minlength="8" ng-required="true" />
 					</div>
+					<div class="alert alert-danger" role="alert" ng-messages="signupForm.password.$error" ng-if="signupForm.password.$touched" ng-hide="signupForm.password.$valid">
+						<p ng-message="minlength">Password must be at least 8 characters.</p>
+						<p ng-message="required">Please enter your password.</p>
+					</div>
 				</div>
 				<div class="form-group" ng-class="{ 'has-error': signupForm.confirmPassword.$touched && signupForm.confirmPassword.$invalid }">
 					<label class="control-label">Confirm Password</label>
@@ -43,11 +54,16 @@
 						<div class="input-group-addon">
 							<i class="fa fa-key" aria-hidden="true"></i>
 						</div>
-						<input type="password" class="form-control" id="confirmPassword" name="confirmPassword" placeholder="Confirm Password&hellip;" ng-model="signupData.confirmPassword" ng-minlength="8" ng-required="true" />
+						<input type="password" class="form-control" id="confirmPassword" name="confirmPassword" placeholder="Confirm Password&hellip;" match-password="password" ng-model="signupData.confirmPassword" ng-minlength="8" ng-required="true" />
+					</div>
+					<div class="alert alert-danger" role="alert" ng-messages="signupForm.confirmPassword.$error" ng-if="signupForm.confirmPassword.$touched" ng-hide="signupForm.confirmPassword.$valid">
+						<p ng-message="minlength">Password must be at least 8 characters.</p>
+						<p ng-message="passwordMatch">Passwords do not match.</p>
+						<p ng-message="required">Please enter your password.</p>
 					</div>
 				</div>
 				<hr />
-				<button type="submit" class="btn btn-lg btn-info"><i class="fa fa-check" aria-hidden="true"></i> Join</button>
+				<button type="submit" class="btn btn-lg btn-info" ng-disabled="signupForm.$invalid"><i class="fa fa-check" aria-hidden="true"></i> Join</button>
 				<button type="reset" class="btn btn-lg btn-warning"><i class="fa fa-ban" aria-hidden="true"></i> Cancel</button>
 			</form>
 			<p>Or, join us with one of the following social networks:</p>
