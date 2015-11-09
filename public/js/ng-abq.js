526a8c926c3b0a736f6ef394999da0d04637c9bf
Dylan McDonald <dmcdonald21@cnm.edu>
2015-11-08 19:03:03 -0700

    Added a different Angular interpolator - since PHP Blade is in conflict with Angular's syntax

--- public/js/ng-abq.js
+++ public/js/ng-abq.js
@@ -1,1 +1,4 @@
-var app = angular.module("NgAbq", ["ngMessages", "ngPassword", "ui.bootstrap"]);
+var app = angular.module("NgAbq", ["ngMessages", "ngPassword", "ui.bootstrap"], function($interpolateProvider) {
+	$interpolateProvider.startSymbol("<%angular");
+	$interpolateProvider.endSymbol("%>");
+});
