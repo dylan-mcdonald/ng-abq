5d8bddabb2dcabe1c82688ed230b4f5b8b2714c2
Dylan McDonald <dmcdonald21@cnm.edu>
2015-11-18 08:36:32 -0700

    Added an alert to emphasize our regular meeting days and venue

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -1,9 +1,13 @@
 @extends("ng-abq")
 @section("content")
 	<h1>Upcoming Events</h1>
+	<h4 class="alert alert-info" role="alert">
+		We regularly meet the first Monday of each month at <a href="http://getplowed.com/site/our-locations/wells-park/">Tractor Brewery, Wells Park</a> at 6:00 pm.
+	</h4>
 	<p>
 		Angular is excited to host the following events. Join us for fun, beer, and Angular conversation!
 	</p>
+	<hr />
 	<div ng-controller="EventController">
 		<div ng-repeat="event in events">
 			<abq-events></abq-events>
