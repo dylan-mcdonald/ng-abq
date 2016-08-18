a04c655b70ea75137603d71f7ed0fedfc9af10fc
eliotostling <it.treugott@gmail.com>
2016-08-19 03:33:32 +0900

    Test push with a little bit of something

---
+++ public_html/lib/Event.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Com\NgAbq\Beta;
+require_once("autoload.php");
+
+/**
+ *
+ * Event class lists when ng events take place
+ * 
+ *
+ * @author Eliot Ostling <it.treugott@gmail.com>
+ *
+ * @version 1.0.0
+ **/
+
+class Event implements \JsonSerializable {
+
+
+/*
+ * Primary Key
+ */
+private $eventId;
+/*
+ * 
+ */
+private $eventProfileId;
+/*
+ *
+ */
+private $eventName;
+/*
+ *
+ */
+private $eventDate;
+/*
+ *
+ */
+private $eventTime;
+
+
+
+
+
+
+
+}
