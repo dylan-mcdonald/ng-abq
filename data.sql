4e5a1ece0671e7db1b27dea6406ad6c424a99d64
Marlan Ball <wyndows2k@earthlink.net>
2016-08-31 11:52:30 -0600

    creating data for database for testing

---
+++ data.sql
@@ -0,0 +1,6 @@
+INSERT INTO profile(profileAdmin, profileNameFirst, profileNameLast, profileEmail, profileUserName) VALUES(1, "billy", "bob", "billybob@silly.com", "billybob");
+INSERT INTO profile(profileAdmin, profileNameFirst, profileNameLast, profileEmail, profileUserName) VALUES(1, "hannah", "sue", "hannahsue@silly.com", "hannahsue");
+INSERT INTO image(imageProfileId, imageFileName, imageType) VALUES(1, "testing1", "jpeg");
+INSERT INTO image(imageProfileId, imageFileName, imageType) VALUES(2, "testing2", "jpeg");
+INSERT INTO link(linkProfileId, linkProfileUserName, linkUrl, linkDate) VALUES(1, "billybob", "sillyquotes.com", "2016-05-12 12:12:12");
+INSERT INTO link(linkProfileId, linkProfileUserName, linkUrl, linkDate) VALUES(1, "hannahsue", "sillierquotes.com", "2016-05-12 12:12:15");
