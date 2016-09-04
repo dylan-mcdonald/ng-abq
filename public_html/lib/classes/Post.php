9aa1aa4bd7f72d3f4dd754696b4b99f956360489
Marlan Ball <wyndows2k@earthlink.net>
2016-09-04 13:32:28 -0600

    working on post class

--- public_html/lib/classes/Post.php
+++ public_html/lib/classes/Post.php
@@ -13,6 +13,55 @@ require_once("autoload.php");
  * @version 1.0.0
  **/
 class Post implements \JsonSerializable {
+
+	/* STATE VARIABLES */
+
+	/**
+	 * Primary key of post
+	 * @var int $postId
+	 **/
+	private $postId;
+
+	/**
+	 * Post profile user name
+	 * @var string $postProfileUserName
+	 **/
+	private $postProfileUserName;
+
+	/**
+	 * Post submission
+	 * @var string $postSubmission
+	 **/
+	private $postSubmission;
+
+	/**
+	 * Post time
+	 * @var \DateTime $postTime
+	 **/
+	private $postTime;
+
+	//construct
+	public function __construct(int $newPostId = null, string $newPostProfileUserName, string $newPostSubmission, string $newPostTime = null) {
+		try {
+			$this->setPostId($newPostId);
+			$this->setPostProfileUserName($newPostProfileUserName);
+			$this->setPostSubmission($newPostSubmission);
+			$this->setPostTime($newPostTime);
+		} catch(\InvalidArgumentException $invalidArgument) {
+			//rethrow the exception to the caller
+			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+		} catch(\RangeException $range) {
+			//rethrow the exception to the caller
+			throw(new \RangeException($range->getMessage(), 0, $range));
+		} catch(\TypeError $typeError) {
+			//rethrow the exception to the caller
+			throw(new \TypeError($typeError->getMessage(), 0, $typeError));
+		} catch(\Exception $exception) {
+			//rethrow the exception to the caller
+			throw(new \Exception($exception->getMessage(), 0, $exception));
+		}
+	}
+
 	//TODO
 	public function jsonSerialize() {
 		// TODO
