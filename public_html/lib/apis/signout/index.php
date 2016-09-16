dcc4b9981b03a493a880ab52a89a9ef1461dccbf
Skyler Rexroad <skyrex1095@gmail.com>
2016-09-16 08:29:16 -0600

    Added signout API

---
+++ public_html/lib/apis/signout/index.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Signout API
+ *
+ * @author Skyler Rexroad <skyler.rexroad@gmail.com>
+ *
+ * @version 1.0.0
+ **/
+
+// Verify the session and start it if it's not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	if ($method === "GET") {
+		$_SESSION = [];
+		$reply->message = "Successfully signed out.";
+	} else {
+		throw new \RuntimeException("Method not allowed.");
+	}
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+	$reply->trace = $exception->getTraceAsString();
+} catch(TypeError $typeError) {
+	$reply->status = $typeError->getCode();
+	$reply->message = $typeError->getMessage();
+}
+
+header("Content-type: application/json");
+
+if($reply->data === null) {
+	unset($reply->data);
+}
+
+echo json_encode($reply);
