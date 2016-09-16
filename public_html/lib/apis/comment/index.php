a281513f2316ca1896facdbd6e32ba8906135f16
Skyler Rexroad <skyrex1095@gmail.com>
2016-09-15 18:44:28 -0600

    Added the beginnings of APIs

--- public_html/lib/apis/comment/index.php
+++ public_html/lib/apis/comment/index.php
@@ -1,1 +1,59 @@
 <?php
+
+/**
+ * Comment API
+ *
+ * @author Skyler Rexroad <skyler.rexroad@gmail.com>
+ *
+ * @version 1.0.0
+ **/
+
+use Com\NgAbq\Beta\Comment;
+
+require_once dirname(__DIR__, 2) . "/classes/autoload.php";
+require_once dirname(__DIR__, 2) . "/lib/xsrf.php";
+require_once("/etc/apache2/encrypted-config/encrypted-config.php");
+
+// Verify the session and start it if it's not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	$config = connectToEncryptedMySQL("/etc/apache2/encrypted-config/ng-abq-dev.ini");
+
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+
+	if (($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
+		throw(new InvalidArgumentException("", 405));
+	}
+
+	if ($method === "GET") {
+		if (empty($id) === false) {
+			$comment = Comment::getCommentByCommentId($pdo, $id);
+			if ($comment !== null) {
+				$reply->data = $comment;
+			}
+		}
+	}
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+	$reply->trace = $exception->getTraceAsString();
+} catch(TypeError $typeError) {
+	$reply->status = $typeError->getCode();
+	$reply->message = $typeError->getMessage();
+}
+
+header("Content-type: application/json");
+if($reply->data === null) {
+	unset($reply->data);
+}
+
+echo json_encode($reply);
--- public_html/lib/apis/post/index.php
+++ public_html/lib/apis/post/index.php
@@ -1,1 +1,59 @@
 <?php
+
+/**
+ * Post API
+ *
+ * @author Skyler Rexroad <skyler.rexroad@gmail.com>
+ *
+ * @version 1.0.0
+ **/
+
+use Com\NgAbq\Beta\Post;
+
+require_once dirname(__DIR__, 2) . "/classes/autoload.php";
+require_once dirname(__DIR__, 2) . "/lib/xsrf.php";
+require_once("/etc/apache2/encrypted-config/encrypted-config.php");
+
+// Verify the session and start it if it's not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	$config = connectToEncryptedMySQL("/etc/apache2/encrypted-config/ng-abq-dev.ini");
+
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+
+	if (($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
+		throw(new InvalidArgumentException("", 405));
+	}
+
+	if ($method === "GET") {
+		if (empty($id) === false) {
+			$post = Post::getPostByPostId($pdo, $id);
+			if ($post !== null) {
+				$reply->data = $post;
+			}
+		}
+	}
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+	$reply->trace = $exception->getTraceAsString();
+} catch(TypeError $typeError) {
+	$reply->status = $typeError->getCode();
+	$reply->message = $typeError->getMessage();
+}
+
+header("Content-type: application/json");
+if($reply->data === null) {
+	unset($reply->data);
+}
+
+echo json_encode($reply);
