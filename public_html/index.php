26c5dd51b3a0bbe10ebebf1235522cb53c9f741e
Skyler Rexroad <skyrex1095@gmail.com>
2016-09-17 13:46:53 -0600

    Added custom CSS

--- public_html/index.php
+++ public_html/index.php
@@ -6,6 +6,9 @@
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="../app/app.css">
+
 		<!-- Boostrap JS -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
