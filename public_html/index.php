a14acdb2de84ac0d9ee94cfc37ad0b90e83e7879
Skyler Rexroad <skyrex1095@gmail.com>
2016-09-17 13:43:29 -0600

    Added Bootstrap, jQuery, and Font Awesome

--- public_html/index.php
+++ public_html/index.php
@@ -2,6 +2,19 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Boostrap JS -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<!-- jQuery -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+
+		<!-- Font Awesome -->
+		<script src="https://use.fontawesome.com/b51868143a.js"></script>
+
 		<title>ng-abq</title>
 	</head>
 	<body>
