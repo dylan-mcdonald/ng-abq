acdc3e08ff53a627840ea625efc192a94a7880f7
Skyler Rexroad <skyrex1095@gmail.com>
2016-09-17 09:13:53 -0600

    Customized app component

--- public_html/app/app.component.ts
+++ public_html/app/app.component.ts
@@ -1,14 +1,10 @@
 import {Component} from "@angular/core";
 
 @Component({
-	selector: 'deepdivedylan-app',
-	templateUrl: './templates/deepdivedylan-app.html'
+	selector: 'ng-abq-app',
+	templateUrl: './templates/app.html'
 })
 
 export class AppComponent {
-	navCollapse = true;
 
-	toggleCollapse() {
-		this.navCollapse = !this.navCollapse;
-	}
 }
