978fe9e511af6f694543bf548ef6d8c57f9fd86d
Skyler Rexroad <skyrex1095@gmail.com>
2016-09-17 00:59:42 -0600

    Loading icon is prettier

--- public_html/index.php
+++ public_html/index.php
@@ -6,6 +6,6 @@
 	</head>
 	<body>
 		<h1>We are under construction.  Come back and visit soon.</h1>
-		<ng-abq-app><i class="fa fa-fw fa-3x fa-circle-o-notch fa-spin"></i><span class="sr-only">Loading...</span></ng-abq-app>
+		<ng-abq-app><i class="fa fa-fw fa-3x fa-circle-o-notch fa-spin text-success"></i><span class="sr-only">Loading...</span></ng-abq-app>
 	</body>
 </html>
