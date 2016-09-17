ce18a1f5dd708dd4ca0a2c825638190bd3cf63e9
Skyler Rexroad <skyrex1095@gmail.com>
2016-09-17 13:38:27 -0600

    Updated wording on the main page

--- public_html/index.php
+++ public_html/index.php
@@ -2,11 +2,10 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-
-		<title>NG-ABQ</title>
+		<title>ng-abq</title>
 	</head>
 	<body>
-		<h1>We are under construction.  Come back and visit soons.</h1>
+		<h1>We are under construction. Come back and visit soon.</h1>
 		<ng-abq-app><i class="fa fa-fw fa-3x fa-circle-o-notch fa-spin text-success"></i><span class="sr-only">Loading...</span></ng-abq-app>
 	</body>
 </html>
