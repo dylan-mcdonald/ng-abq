98faabf992ce4b224f3f021c165b73bb6b763e08
Dylan McDonald <dmcdonald21@cnm.edu>
2016-10-01 15:39:50 -0600

    construction destruction

--- webpack/index.php
+++ webpack/index.php
@@ -16,22 +16,13 @@ setXsrfCookie();
 		<title>ng-abq</title>
 	</head>
 	<body>
-		<div class="under-construction">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12">
-						<h2 class="text-center">We are under construction. Come back and visit soon.</h2>
-					</div>
-				</div>
-			</div>
-		</div>
 		<ng-abq-app>
 			<div class="container">
 				<div class="row">
 					<div class="col-md-12">
 						<div class="progress site-loading-bar">
 							<div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
-								Loading site...
+								Loading site&hellip;
 							</div>
 						</div>
 					</div>
