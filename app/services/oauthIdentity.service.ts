10e0de2ab941dc683a7bbb0e72a4006f50229fb8
Skyler Rexroad <skyrex1095@gmail.com>
2016-10-01 13:15:37 -0600

    Added OauthIdentity service

---
+++ app/services/oauthIdentity.service.ts
@@ -0,0 +1,45 @@
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+import {Observable} from "rxjs/Observable";
+import {BaseService} from "./base.service";
+import {OauthIdentity} from "../classes/oauthIdentity";
+import {Status} from "../classes/status";
+
+@Injectable()
+export class OauthIdentityService extends BaseService {
+	constructor(protected http: Http) {
+		super(http);
+	}
+
+	private oauthIdentityUrl = "api/oauthIdentity/";
+
+	deleteOauthIdentity(oauthIdentityId: number) : Observable<Status> {
+		return(this.http.delete(this.oauthIdentityUrl + oauthIdentityId)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+
+	getAllOauthIdentitys() : Observable<OauthIdentity[]> {
+		return(this.http.get(this.oauthIdentityUrl)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+
+	getOauthIdentity(oauthIdentityId: number) : Observable<OauthIdentity> {
+		return(this.http.get(this.oauthIdentityUrl + oauthIdentityId)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+
+	createOauthIdentity(oauthIdentity: OauthIdentity) : Observable<Status> {
+		return(this.http.post(this.oauthIdentityUrl, oauthIdentity)
+			.map(this.extractMessage)
+			.catch(this.handleError));
+	}
+
+	editOauthIdentity(oauthIdentity: OauthIdentity) : Observable<Status> {
+		return(this.http.put(this.oauthIdentityUrl + oauthIdentity.oauthIdentityId, oauthIdentity)
+			.map(this.extractMessage)
+			.catch(this.handleError));
+	}
+}
