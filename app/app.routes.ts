1d3f8cef6d135599dc173f2f9790b26ce4374512
Skyler Rexroad <skyrex1095@gmail.com>
2016-10-01 13:40:11 -0600

    Updated calendar to event

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,5 +1,5 @@
 import {RouterModule, Routes} from "@angular/router";
-import {CalendarComponent} from "./components/calendar.component";
+import {EventComponent} from "./components/event.component";
 import {HeaderComponent} from "./components/header.component";
 import {IntroComponent} from "./components/intro.component";
 import {LoginComponent} from "./components/login.component";
@@ -7,10 +7,10 @@ import {NewsComponent} from "./components/news.component";
 import {SignupComponent} from "./components/signup.component";
 
 
-export const allAppComponents = [CalendarComponent, HeaderComponent, IntroComponent, LoginComponent, NewsComponent, SignupComponent];
+export const allAppComponents = [EventComponent, HeaderComponent, IntroComponent, LoginComponent, NewsComponent, SignupComponent];
 
 export const routes: Routes = [
-	{path: "calendar", component: CalendarComponent},
+	{path: "event", component: EventComponent},
 	{path: "header", component: HeaderComponent},
 	{path: "login", component: LoginComponent},
 	{path: "news", component: NewsComponent},
