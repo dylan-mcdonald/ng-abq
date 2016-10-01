06d8215a358f9c7774a342e7c0c85793cd1f9572
Dylan McDonald <dmcdonald21@cnm.edu>
2016-10-01 13:05:36 -0600

    corrected pathing

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({
 	selector: 'ng-bq-app',
-	templateUrl: './templates/app.html'
+	templateUrl: './templates/app.php'
 })
 
 export class AppComponent {
