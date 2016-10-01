2099cc250ed7905f97b1a895459018a0e043c574
Dylan McDonald <dmcdonald21@cnm.edu>
2016-10-01 13:10:24 -0600

    updated webpack configuration

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -18,16 +18,20 @@ module.exports = {
 	module: {
 		loaders: [
 			{
-				test: /\.ts$/,
-				loaders: ['awesome-typescript-loader', 'angular2-template-loader']
-			},
-			{
 				test: /\.html$/,
 				loader: 'html'
 			},
 			{
 				test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
 				loader: 'file?name=assets/[name].[hash].[ext]'
+			},
+			{
+				test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: "url-loader?name=/[name].[hash].[ext]&limit=10000&mimetype=application/font-woff"
+			},
+			{
+				test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: "file-loader?name=/[name].[hash].[ext]"
 			},
 			{
 				test: /\.css$/,
@@ -38,6 +42,10 @@ module.exports = {
 				test: /\.css$/,
 				include: helpers.root('src', 'app'),
 				loader: 'raw'
+			},
+			{
+				test: /\.ts$/,
+				loaders: ['ts', 'angular2-template-loader']
 			}
 		]
 	},
