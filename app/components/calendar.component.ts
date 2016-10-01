1924235e9a540856a3b3c0b5efe847e6f5f7616b
Skyler Rexroad <skyrex1095@gmail.com>
2016-10-01 12:54:47 -0600

    Updated Calendar Component

--- app/components/calendar.component.ts
+++ app/components/calendar.component.ts
@@ -1,41 +1,6 @@
-import {Component} from '@angular/core';
+import {Component} from "@angular/core";
 
 @Component({
-	selector: 'calendar',
-	templateUrl: 'app/templates/calendar.component.html',
-	styleUrls: ['app/app.css']
+	templateUrl: "./templates/calendar.html"
 })
-
-export class CalendarComponent {
-
-	constructor(private http: Http) {}
-
-
-	getCalendar() :  Observable<CalendarData[]>   {
-
-		return this.http.get('../lib/apis/event/index.php').map(this.extractData).catch(this.handleError);
-
-	}
-
-	extractData (response: Response) {
-
-		if(response.status < 200 || response.status >= 300) {
-			throw(new Error("Bad response status: " + response.status));
-		}
-
-		let reply = [];
-		let body = response.json();
-		body.forEach(function(item) {
-			let clndr = {eventName: item.FIELD1, eventTime: item.FIELD2};
-			reply.push(clndr);
-		});
-		return(reply);
-
-	}
-
-	private handleError(error: any) {
-		let message = error.message;
-		console.log(message);
-		return(Observable.throw(message));
-	}
-}
+export class CalendarComponent {}
