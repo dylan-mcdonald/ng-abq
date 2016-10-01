d8b09d44bae5375742b575e0776574a77e83d2d8
Skyler Rexroad <skyrex1095@gmail.com>
2016-10-01 15:50:39 -0600

    Finished(?) Calendar Component

--- app/components/calendar.component.ts
+++ app/components/calendar.component.ts
@@ -1,6 +1,59 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {Router, ActivatedRoute, Params} from "@angular/router";
+import {EventService} from "../services/event.service";
+import {Event} from "../classes/event";
+import {Status} from "../classes/status";
 
 @Component({
 	templateUrl: "./templates/calendar.php"
 })
-export class CalendarComponent {}
+export class CalendarComponent implements OnInit {
+	deleted: boolean = false;
+	events: Event[] = [];
+	event: Event = new Event(null, null, null, null);
+	status: Status = null;
+
+	constructor(private eventService: EventService, private router: Router, private route: ActivatedRoute) {}
+
+	ngOnInit(): void {
+		this.route.params.forEach((params : Params) => {
+			let eventId = +params["eventId"];
+			this.eventService.getEvent(eventId)
+				.subscribe(event => this.event = event);
+		});
+		this.reloadEvents();
+	}
+
+	createEvent() : void {
+		this.eventService.createEvent(this.event)
+			.subscribe(status => {
+				this.status = status;
+				if(status.status === 200) {
+					this.reloadEvents();
+				}
+			});
+	}
+
+	deleteEvent() : void {
+		this.eventService.deleteEvent(this.event.eventId)
+			.subscribe(status => {
+				this.deleted = true;
+				this.status = status;
+				this.event = new Event(null, null, null, null);
+			});
+	}
+
+	editEvent() : void {
+		this.eventService.editEvent(this.event)
+			.subscribe(status => this.status = status);
+	}
+
+	reloadEvents() : void {
+		this.eventService.getAllEvents()
+			.subscribe(events => this.events = events);
+	}
+
+	switchEvent(event : Event) : void {
+		this.router.navigate(["/event/", event.eventId]);
+	}
+}
