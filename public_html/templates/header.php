e6dbda8d6c2000fdb52ac747386dff60573e1998
Skyler Rexroad <skyrex1095@gmail.com>
2016-10-01 14:58:14 -0600

    Updated navbar

--- public_html/templates/header.php
+++ public_html/templates/header.php
@@ -1,1 +1,24 @@
-<h2>This is a navbar. Dang.</h2>
+<nav class="navbar navbar-default">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <i class="fa fa-bars"></i>
+      </button>
+      <a class="navbar-brand" href="#"><code>ng-abq</code></a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a routerLink="/home">Home</a></li>
+        <li><a routerLink="/members">Members</a></li>
+		<li><a routerLink="/discuss">Discuss</a></li>
+		<li><a routerLink="/photos">Photos</a></li>
+		<li><a routerLink="/files">Files</a></li>
+		<li><a routerLink="/calendar">Calendar</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
