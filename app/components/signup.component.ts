233dc66980ccaffff2814990f9ee7deebae9c77c
eliotostling <it.treugott@gmail.com>
2016-10-26 05:20:27 +0900

    attempting signup component

--- app/components/signup.component.ts
+++ app/components/signup.component.ts
@@ -3,5 +3,12 @@ import {Component} from "@angular/core";
 @Component({
 	templateUrl: "./templates/signup.php"
 })
-
+//Trying to create a signup component not even close to being finished.
 export class SignupComponent {}
+@ViewChild("addSignupForm") addSignupForm;
+
+signup: Profile[] = [];
+profile: Profile = new Profile(0, 0, "", "", "", "", "", "", "");
+status: Status = null;
+
+constructor(){}
